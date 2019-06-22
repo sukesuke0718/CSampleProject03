953f4ad78c339c07bad78619162cedb443e61472
ユウ <sukesuke0718@gmail.com>
2019-06-23 03:47:07 +0900

    test

--- SampleProject02/sample01.c
+++ SampleProject02/sample01.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
-main()
-{
-	printf("Hello, World\n");
+
+int	main(void){
+
+	int number;
+	char moji[12] = "Hello,World";
+
+	printf("input number:");
+	scanf("%d", &number);
+
+	if (number > 0) {
+		for (int i = 0; i <= number; i++) {
+			printf("%d", i + 1);
+			printf(" : ");
+			printf("%s\n", moji);
+		}
+	}
+	else {
+		printf("miss");
+	}
+
 	return 1;
 }
